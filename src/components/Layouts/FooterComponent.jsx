f0b674c779432ba589caf68726356e317afdccc8
WilsonWang <iloveldt110323@gmail.com>
2021-12-25 23:27:41 +1100

    modify footer css

--- src/components/Layouts/FooterComponent.jsx
+++ src/components/Layouts/FooterComponent.jsx
@@ -1,8 +1,10 @@
 const FooterComponent = (props) => {
 	return (
-		<footer className="footer">
-			<span className="text-muted">All Rights Reserved 2021 @WW</span>
-		</footer>
+		<div className="container">
+			<footer className="footer footer-copyright">
+				<span className="text-muted">All Rights Reserved 2021 @WW</span>
+			</footer>
+		</div>
 	);
 };
 
