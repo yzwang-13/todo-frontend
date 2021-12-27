efa5afa12ac78b219d4d775e938784ce28bbc9d9
WilsonWang <iloveldt110323@gmail.com>
2021-12-27 22:52:32 +1100

    add todoRowcomponent

--- src/components/Pages/ListTodosComponent.jsx
+++ src/components/Pages/ListTodosComponent.jsx
@@ -1,63 +1,60 @@
 import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router";
 import Spinner from "../utils/Spinner/Spinner";
+import useTodos from "./hooks/useTodos";
 import ErrorService from "./services/ErrorService";
 import TodoService from "./services/TodoService";
+import TodoRowComponent from "./TodoRowComponent";
 
 const ListTodosComponent = (props) => {
+	const navigate = useNavigate();
+	const todos = useTodos().todos;
+	const initializeTodos = useTodos().initializeTodos;
+
 	console.log("ListTodosComponent");
 
-	const [todos, setTodos] = useState([]);
+	// const [todos, setTodos] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [message, setMessage] = useState(null);
 
 	useEffect(() => {
 		console.log("useEffect");
-		refreshTodos();
+		fetchTodos();
 	}, []);
+
+	const updateTodoHander = (id) => {
+		navigate(`/todo/${id}`);
+	};
 
 	const deleteTodoHandler = (id) => {
 		TodoService.deleteTodo(id)
 			.then((response) => {
 				setMessage("Delete message successful");
 				console.log(response);
-				refreshTodos();
+				fetchTodos();
 			})
 			.catch((error) => {
 				ErrorService.handleError(error);
 			});
 	};
 
-	const refreshTodos = async () => {
-		setTimeout(() => {
-			setIsLoading(true);
-
-			TodoService.getListofTodos()
-				.then((response) => {
-					setTimeout(() => {
-						// const newTodos = response.data.foreach((todo) => {
-						// 	console.log(todo);
-						// 	const container = {};
-						// 	container.id = todo.id;
-						// 	container.description = todo.description;
-						// 	container.done = false;
-						// 	container.targetDate = todo.due;
-						// 	return container;
-						// });
-						const newTodos = [...response.data];
-						const transformedTodo = newTodos.map((todo) => {
-							return {
-								id: todo.id,
-								description: todo.description,
-								done: todo.done,
-								targetDate: todo.targetDate
-							};
-						});
-						setTodos(transformedTodo);
-						setIsLoading(false);
-					}, 1000);
-				})
-				.catch((error) => ErrorService.handleError(error));
-		}, 1000);
+	const fetchTodos = () => {
+		setIsLoading(true);
+		TodoService.getListofTodos()
+			.then((response) => {
+				const newTodos = [...response.data];
+				const transformedTodo = newTodos.map((todo) => {
+					return {
+						id: todo.id,
+						description: todo.description,
+						done: todo.done,
+						targetDate: todo.targetDate
+					};
+				});
+				initializeTodos(transformedTodo);
+				setIsLoading(false);
+			})
+			.catch((error) => ErrorService.handleError(error));
 	};
 
 	const outputTodosTableBodyRows = () => {
@@ -65,20 +62,12 @@ const ListTodosComponent = (props) => {
 			return [
 				...todos.map((todo) => {
 					return (
-						<tr key={todo.id}>
-							<td>{todo.id}</td>
-							<td>{todo.description}</td>
-							<td>{todo.done.toString()}</td>
-							<td>{todo.targetDate}</td>
-							<td>
-								<button
-									className="btn btn-sm btn-outline-danger"
-									onClick={() => deleteTodoHandler(todo.id)}
-								>
-									DELETE
-								</button>
-							</td>
-						</tr>
+						<TodoRowComponent
+							key={todo.id}
+							todo={todo}
+							updateTodoHander={updateTodoHander}
+							deleteTodoHandler={deleteTodoHandler}
+						/>
 					);
 				})
 			];
@@ -116,7 +105,8 @@ const ListTodosComponent = (props) => {
 									<th>Description</th>
 									<th>Is completed?</th>
 									<th>Target Date</th>
-									<th>Delete?</th>
+									<th>Update</th>
+									<th>Delete</th>
 								</tr>
 							</thead>
 							<tbody>{outputTodosTableBodyRows()}</tbody>
