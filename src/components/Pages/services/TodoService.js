5621d68a86613a1dee8082fc1dd01f5b1a82af8b
WilsonWang <iloveldt110323@gmail.com>
2021-12-27 22:56:57 +1100

    add put request

--- src/components/Pages/services/TodoService.js
+++ src/components/Pages/services/TodoService.js
@@ -14,4 +14,12 @@ export default class TodoService {
 			process.env.REACT_APP_BASE_URL_DEV + `/todos/wilsonwang/${id}`
 		);
 	}
+
+	static updateTodo(id, todo) {
+		// console.log(process.env.REACT_APP_BASE_URL_DEV + "/todos/wilsonwang");
+		return axios.put(
+			process.env.REACT_APP_BASE_URL_DEV + `/todos/wilsonwang/${id}`,
+			todo
+		);
+	}
 }
