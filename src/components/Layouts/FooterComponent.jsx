602283b7f7b0e5b8806aad8945e89c98d0e380ea
WilsonWang <iloveldt110323@gmail.com>
2021-12-27 22:50:55 +1100

    remove footer for now

--- src/components/Layouts/FooterComponent.jsx
+++ src/components/Layouts/FooterComponent.jsx
@@ -1,9 +1,9 @@
 const FooterComponent = (props) => {
 	return (
 		<div className="container">
-			<footer className="footer footer-copyright">
+			{/* <footer className="footer footer-copyright">
 				<span className="text-muted">All Rights Reserved 2021 @WW</span>
-			</footer>
+			</footer> */}
 		</div>
 	);
 };
